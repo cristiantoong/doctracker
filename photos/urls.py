94f88b49e5986baa7eba4b28b2701d91caed5935
toongcristian01 <toongcristian01@gmail.com>
2024-09-08 00:21:09 +0800

    Initial commit of Django project

---
+++ photos/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.photo_gallery, name='photo_gallery'),
+    path('upload-photo/', views.upload_photo, name='upload_photo'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ photos/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from .forms import PhotoForm
+from .models import Photo
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def upload_photo(request):
+    if request.method == 'POST':
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.user = request.user
+            photo.save()
+            return redirect('photo_gallery')
+    else:
+        form = PhotoForm()
+    return render(request, 'photos/post/upload_photo.html', {'form': form})
+
+def photo_gallery(request):
+    photos = Photo.objects.filter(user=request.user)
+    return render(request, 'photos/post/gallery.html', {'photos': photos})
